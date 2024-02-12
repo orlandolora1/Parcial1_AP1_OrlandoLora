a938f741e4829e6970e594b17b7d5a974a412e4a
orlandolora1 <loraorlando3713@gmail.com>
2024-02-12 18:28:03 -0400

    Agregando Modelos Metas

---
+++ Models/Metas.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_AP1_OrlandoLora.Models
+{
+	public class Metas
+	{
+		[Key]
+		public int MetaId { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime Fecha { get; set; } = DateTime.Today;
+
+		[Required(ErrorMessage = "Debe ingresar un nombre")]
+		[RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "El nombre no debe contener números ni caracteres especiales")]
+		public string Persona { get; set; }
+
+		[Required(ErrorMessage = "Debe ingresar una observación")]
+		public string Observacion { get; set; }
+
+		[Range(1, int.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+		public decimal Monto { get; set; }
+	}
+}
+
+
+
+
